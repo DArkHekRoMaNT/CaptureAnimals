02cae9e669821439c44d344d0511894f3df25651
DArkHekRoMaNT <DArkHekRoMaNT@yandex.ru>
2020-06-23 05:06:12 +0300

    v1, build #3

--- src/PackingAnimals.cs
+++ src/PackingAnimals.cs
@@ -1,11 +1,12 @@
-﻿using Vintagestory.API.Common;
-using System;
+﻿using System;
+using Vintagestory.API.Common;
 
 namespace PackingAnimals
 {
     public class PackingAnimals : ModSystem
     {
         public static string MOD_ID = "packinganimals";
+        public static string MOD_SPACE = "PackingAnimals";
 
         public override void Start(ICoreAPI api)
         {
@@ -17,8 +18,7 @@ namespace PackingAnimals
 
             foreach (string e in items)
             {
-                api.RegisterItemClass(e, Type.GetType(e));
-                api.World.Logger.Debug("Registered [" + e + "]");
+                api.RegisterItemClass(e, Type.GetType(MOD_SPACE + "." + e));
             }
         }
     }
