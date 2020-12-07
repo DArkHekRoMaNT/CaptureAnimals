0aa214dac18ec6e19da09d46e5c5cf9f88ec6a49
DArkHekRoMaNT <darkhekromant@yandex.ru>
2020-12-07 11:42:26 +0300

    Update to 1.14.0 (still works on 1.13.4)

--- src/CaptureAnimals.cs
+++ src/CaptureAnimals.cs
@@ -1,23 +1,15 @@
-﻿using System;
-using Vintagestory.API.Common;
-
-[assembly: ModInfo("CaptureAnimals")]
+﻿using Vintagestory.API.Common;
 
 namespace CaptureAnimals
 {
     public class CaptureAnimals : ModSystem
     {
-        public static string MOD_ID = "captureanimals";
-        public static string MOD_SPACE = "CaptureAnimals";
-
         public override void Start(ICoreAPI api)
         {
             base.Start(api);
 
-            api.RegisterItemClass("ItemCage", Type.GetType(MOD_SPACE + ".ItemCage"));
-            api.RegisterBlockClass("BlockCage", Type.GetType(MOD_SPACE + ".BlockCage"));
-            api.RegisterEntity("EntityThrownCage", Type.GetType(MOD_SPACE + ".EntityThrownCage"));
-            api.RegisterBlockEntityClass("BlockEntityCage", Type.GetType(MOD_SPACE + ".BlockEntityCage"));
+            api.RegisterItemClass("ItemCage", typeof(ItemCage));
+            api.RegisterEntity("EntityThrownCage", typeof(EntityThrownCage));
         }
     }
 }
---
+++ src/Contants.cs
@@ -0,0 +1,7 @@
+namespace CaptureAnimals
+{
+    public static class Constants
+    {
+        public const string MOD_ID = "captureanimals";
+    }
+}
